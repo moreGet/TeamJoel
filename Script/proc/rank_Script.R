58497380e8a4fdba45daa9dc18dc9a28e156024c
Get <dkdlwmzhs@naver.com>
2019-12-04 00:00:03 +0900

    123

--- Script/proc/rank_Script.R
+++ Script/proc/rank_Script.R
@@ -1,5 +1,7 @@
+library(xlsx)
+
 dir <- getwd()
-dir_db <- "../Desktop/Git/TeamJoel/DB/"
+dir_db <- "../Desktop/TeamJoel/DB"
 # setwd("../")
 
 ## Var
@@ -15,22 +17,42 @@ if (dir != dir_db) {
 }
 
 # Data Input
-df <- read.csv(file = "rank.csv", header = F, stringsAsFactors = F)
-head(df, 5)
+df <- read.xlsx(file = "외국인관광객.xlsx", sheetIndex = 1, encoding = enco)
+head(df, 24)
+totalRow <-  floor(nrow(df) / 12)
+yearTemp <- 2003
+
+s <- 1
+e <- 12
+
+for (rowIdx in 1:totalRow) { # NA행에 년도 다 넣기
+  for (idx in seq(s, e, 1)) {
+    if (is.na(df$년[idx])) {
+      df$년[idx] <- yearTemp
+    } else {
+      
+    }
+  }
+  
+  # temp <- e
+  s <- e # index 스위치
+  e <- e + 12 # 고정계수 12
+  
+  yearTemp <- yearTemp + 1 # 년도 더하기기
+}
+
+# 확인
 colnames(df)
+table(is.na(df))
+View(df)
 
-val_Option <- df[1:3, ]
-str(val_Option)
-colnames(val_Option) # 컬럼 명 일치 하긴 함.
 
-val_Option <- val_Option[, -2]
-colnames(val_Option)
 
-# 데이터 중간 저장
-write.csv(x = val_Option, file = "../DB/rankTotal.csv", quote = F)
-
-# 중간 데이터 read
-df <- read.csv(file = "rankTotal.csv", header = T, stringsAsFactors = F)
-df
+# # 데이터 중간 저장
+# write.csv(x = val_Option, file = "../DB/rankTotal.csv", quote = F)
+# 
+# # 중간 데이터 read
+# df <- read.csv(file = "rankTotal.csv", header = T, stringsAsFactors = F)
+# df
 
 # 
